97289197135c0757e0778a2f40d5921f605534fb
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-05-03 16:57:09 +0300

    Updates

---
+++ precache-manifest.1c4bdb09593ed379f48190c15eca7ea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8164dc382d429c672e0991f8913305c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8906bf97abce9d3ffee6",
+    "url": "./static/css/2.dcfccaf9.chunk.css"
+  },
+  {
+    "revision": "fbfd676794d558020c3d",
+    "url": "./static/css/main.68eff11c.chunk.css"
+  },
+  {
+    "revision": "8906bf97abce9d3ffee6",
+    "url": "./static/js/2.1d55b5aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1d55b5aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbfd676794d558020c3d",
+    "url": "./static/js/main.ddf9ef1a.chunk.js"
+  },
+  {
+    "revision": "838d5afca3df2811104a",
+    "url": "./static/js/runtime-main.54764d7d.js"
+  }
+]);
